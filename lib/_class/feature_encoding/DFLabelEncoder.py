6aec5dd82b082716619561b56f1ce15eb24cc71d
Hann-THL <hann_lim@hotmail.com>
2020-04-19 21:18:46 +0800

    remove unnecessary import

--- lib/_class/feature_encoding/DFLabelEncoder.py
+++ lib/_class/feature_encoding/DFLabelEncoder.py
@@ -1,7 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.exceptions import NotFittedError
 from sklearn.preprocessing import LabelEncoder
-import pandas as pd
 
 class DFLabelEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, columns=None):
