e8549a525d21e9cf42155191fd835864af633d9a
Hann-THL <hann_lim@hotmail.com>
2020-04-25 12:26:18 +0800

    remove max VIF identification to fit() function, to ensure transform() result is static after fit

--- lib/_class/feature_selection/DFVIFThreshold.py
+++ lib/_class/feature_selection/DFVIFThreshold.py
@@ -12,19 +12,16 @@ class DFVIFThreshold(BaseEstimator, TransformerMixin):
         self.exclude_const  = exclude_const
         self.show_progress  = show_progress
         self.transform_cols = None
+        self.stat_df        = None
         
     def fit(self, X, y=None):
         self.columns        = X.columns if self.columns is None else self.columns
         self.transform_cols = [x for x in X.columns if x in self.columns]
 
-        return self
-    
-    def transform(self, X):
-        if self.transform_cols is None:
-            raise NotFittedError(f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
-
         # Reference: https://www.statisticshowto.com/variance-inflation-factor/
-        tmp_X = X.copy()
+        tmp_X     = X.copy()
+        stats     = []
+        iteration = 1
         while 1:
             vif_df  = self.calc_vif(tmp_X)
             index   = vif_df[~vif_df.index.isin(['const'])]['VIF'].idxmax()
@@ -33,11 +30,23 @@ class DFVIFThreshold(BaseEstimator, TransformerMixin):
             if max_vif <= self.threshold:
                 break
             tmp_X.drop(columns=[index], inplace=True)
+            stats.append((iteration, index, max_vif))
 
             if self.show_progress:
-                print(f'Removed: {index} ({max_vif :.5f})')
+                print(f'Iteration {iteration}: {index} ({max_vif :.5f})')
+            iteration += 1
 
-        new_X = X[[x for x in tmp_X.columns if x in X.columns]].copy()
+        self.stat_df = pd.DataFrame(stats, columns=['iteration', 'removed', 'vif'])
+
+        return self
+    
+    def transform(self, X):
+        if self.transform_cols is None:
+            raise NotFittedError(f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
+
+        new_X = X.copy()
+        new_X.drop(columns=self.stat_df['removed'].values, inplace=True)
+
         return new_X
     
     def fit_transform(self, X, y=None):
