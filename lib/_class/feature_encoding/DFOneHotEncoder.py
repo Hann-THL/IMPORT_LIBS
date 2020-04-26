37dd797dec30e6ffbca89cb5eebaa178e52e00da
Hann-THL <hann_lim@hotmail.com>
2020-04-26 12:37:34 +0800

    fixed bug for drop='first'

--- lib/_class/feature_encoding/DFOneHotEncoder.py
+++ lib/_class/feature_encoding/DFOneHotEncoder.py
@@ -21,6 +21,8 @@ class DFOneHotEncoder(BaseEstimator, TransformerMixin):
             raise NotFittedError(f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
 
         drops = self.model.drop
+        drops = [x[0] for x in self.model.categories_] if drops == 'first' else drops
+
         new_X = pd.DataFrame(
             self.model.transform(X[self.transform_cols]).toarray(),
             columns=[f'{self.transform_cols[i]}_{y}' for i,x in enumerate(self.model.categories_) for y in x if (drops is None or y != drops[i])]
