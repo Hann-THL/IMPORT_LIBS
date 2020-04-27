034bdfdb53b6cd244b7c35762df94cd461ae3db6
Hann-THL <hann_lim@hotmail.com>
2020-04-27 23:03:35 +0800

    add more hyperparameter notes

--- lib/_class/feature_extraction/DFIvis.py
+++ lib/_class/feature_extraction/DFIvis.py
@@ -16,8 +16,32 @@ import pandas as pd
 # 100K-500K       15-150    5-10                         maaten
 # 500K-1M         15-150    3-5                          szubert
 # > 1M            15-150    2-3                          szubert
+
+# model     hidden layer
+# -------------------------------
+# szubert   128  -> 128  -> 128
+# hinton    2000 -> 1000 -> 500
+# maaten    500  -> 500  -> 2000
+
+# supervision_metric
+# ------------------
+# - Classification:
+#   - binary_crossentropy, hinge, squared_hinge
+# - Classification (Multi-Class):
+#   - sparse_categorical_crossentropy, categorical_crossentropy, categorical_hinge
+# - Regression:
+#   - mean_squared_error, mean_absolute_error, mean_absolute_percentage_error, mean_squared_logarithmic_error
+
+# distance
+# ---------------------------------
+# - euclidean, pn
+# - softmax_ratio, softmax_ratio_pn
+# - manhattan, manhattan_pn
+# - chebyshev, chebyshev_pn
+# - cosine, cosine_pn
+
 class DFIvis(BaseEstimator, TransformerMixin):
-	# NOTE:
+    # NOTE:
     # - DFIvis(embedding_dims=df.shape[1]) to remain every dimensions
     def __init__(self, columns=None, prefix='ivis_', **kwargs):
         self.columns        = columns
